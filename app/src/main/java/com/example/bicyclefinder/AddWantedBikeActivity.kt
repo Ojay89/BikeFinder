3ed735bc08fcf63e4bb94eca91032d340bdd5613
Omar Jaber <48085067+Ojay89@users.noreply.github.com>
2020-11-02 14:51:47 +0100

    Done done - godkendt af Anders

--- app/src/main/java/com/example/bicyclefinder/AddWantedBikeActivity.kt
+++ app/src/main/java/com/example/bicyclefinder/AddWantedBikeActivity.kt
@@ -14,6 +14,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class AddWantedBikeActivity : AppCompatActivity(), OnItemSelectedListener {
     private var progressBar: ProgressBar? = null
     private var messageView: TextView? = null
